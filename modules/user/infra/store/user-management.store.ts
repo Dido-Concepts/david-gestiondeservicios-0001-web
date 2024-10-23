267f756babfc0441d2a9ed676816107e5e8d18c4
jeffri <70425379@udaff.edu.pe>
2024-10-22 20:36:38 -0500

    feat: haciendo mejoras del modal y usuario

--- modules/user/infra/store/user-management.store.ts
+++ modules/user/infra/store/user-management.store.ts
@@ -1,14 +1,19 @@
 import { create } from 'zustand'
+import { ListUsersResponse } from '../../application/use-cases/query/list-users/list-users.response'
 
 interface ModalUserFormState {
   isModalOpen: boolean;
-  toggleModal: () => void;
+  user: ListUsersResponse | null;
+  toggleModal: (user?: ListUsersResponse | null) => void;
 }
 
 export const useModalUserForm = create<ModalUserFormState>((set) => ({
   isModalOpen: false,
-  toggleModal: () =>
+  user: null,
+  toggleModal: (user: ListUsersResponse | null = null) =>
     set((state) => ({
-      isModalOpen: !state.isModalOpen
+      isModalOpen: !state.isModalOpen,
+      user
     }))
+
 }))
