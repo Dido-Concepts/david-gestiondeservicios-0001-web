105470a93f8214844be97afd83894eaae6cd4737
jeffri <48918427+JeffriCM@users.noreply.github.com>
2025-06-09 15:19:26 -0500

    feat: implementar gestión de turnos con prefetch de localizaciones
    - Se agregó prefetch para el catálogo de localizaciones al cargar la página de gestión de turnos.
    - Se incorporó el componente FilterSede con soporte para carga asíncrona.
    - Se envolvió el contenido en HydrationBoundary para manejar la hidratación de React Query.

--- app/dashboard/user-management/shift-management/page.tsx
+++ app/dashboard/user-management/shift-management/page.tsx
@@ -1,16 +1,39 @@
 import ShiftManagementContainer from '@/app/dashboard/user-management/components/ShiftManagementContainer.component'
+import { getQueryClient } from '@/app/providers/GetQueryClient'
+import { getLocationsCatalog } from '@/modules/location/application/actions/location.action'
+import { QUERY_KEYS_LOCATION_MANAGEMENT } from '@/modules/share/infra/constants/query-keys.constant'
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
+import { Suspense } from 'react'
+import { FilterSede } from '@/app/dashboard/service-management/components/FilterSede.component'
 
-export default function ShiftManagementPage () {
+export default async function ShiftManagementPage (props: {
+  searchParams?: Promise<{
+    locationFilter?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams
+  const locationFilter = searchParams?.locationFilter || '0'
+
+  const queryClient = getQueryClient()
+  queryClient.prefetchQuery({
+    queryKey: [QUERY_KEYS_LOCATION_MANAGEMENT.LMGetLocationCatalog],
+    queryFn: () => getLocationsCatalog()
+  })
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-4">
         <div>
           <h1 className="text-3xl md:text-4xl font-bold text-app-quaternary mb-2">Gestionar turnos</h1>
         </div>
+        <Suspense key={locationFilter} fallback={<span>Cargando...</span>}>
+              <FilterSede />
+        </Suspense>
+
       </div>
 
       {/* Contenedor que maneja la selección de semana y la tabla */}
       <ShiftManagementContainer />
-    </>
+    </HydrationBoundary>
   )
 }
